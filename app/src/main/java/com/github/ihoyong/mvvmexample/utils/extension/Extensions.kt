1a38e969e581fd5a8fd49457ef4137ac55a6c4de
leehoyong <lee199402@gmail.com>
2018-09-06 23:42:03 +0900

    * add viewModelProvider extentions

--- app/src/main/java/com/github/ihoyong/mvvmexample/utils/extension/Extensions.kt
+++ app/src/main/java/com/github/ihoyong/mvvmexample/utils/extension/Extensions.kt
@@ -3,6 +3,15 @@ package com.github.ihoyong.mvvmexample.utils.extension
 import android.content.ContextWrapper
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+
+inline fun <reified VM : ViewModel> AppCompatActivity.viewModelProvider(
+        provider: ViewModelProvider.Factory
+) =
+        ViewModelProviders.of(this, provider).get(VM::class.java)
 
 fun View.getParentActivity(): AppCompatActivity? {
     var context = this.context
